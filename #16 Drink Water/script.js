9d1f7802e9518f17afb23fcfe22288be9fc92812
Aditya Kamble <68722478+Aditya6101@users.noreply.github.com>
2021-01-14 17:56:25 +0530

    Add files via upload

---
+++ #16 Drink Water/script.js
@@ -0,0 +1,49 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const listers = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+updateCup();
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener("click", () => fillCups(index));
+});
+
+function fillCups(index) {
+  if (
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("full")
+  ) {
+    index--;
+  }
+
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= index) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+    updateCup();
+  });
+}
+function updateCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    listers.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
